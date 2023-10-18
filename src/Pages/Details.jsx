4c108a91856db9c560ed649b40e7028044e2423a
Paul Prince <39607967+paulprince24542@users.noreply.github.com>
2023-10-18 14:46:56 +0530

    JWT updated

--- src/Pages/Details.jsx
+++ src/Pages/Details.jsx
@@ -97,6 +97,8 @@ export default function Details() {
           .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
+
+            console.log("j_key", credential.idToken)
             const token = credential.accessToken;
             // The signed-in user info.
             const user = result.user;
